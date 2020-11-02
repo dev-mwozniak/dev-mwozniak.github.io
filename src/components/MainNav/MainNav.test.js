dce9beac74ac002b19e91e950c48befda0db926c
Maciej Wozniak <maciej.wozniak@idea07.pl>
2020-11-02 22:22:54 +0100

    dodanie .gitignore

--- src/components/MainNav/MainNav.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import MainNav from './MainNav';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<MainNav />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/components/TvMusic/Tv/Tv.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Tv from './Tv';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Tv />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
